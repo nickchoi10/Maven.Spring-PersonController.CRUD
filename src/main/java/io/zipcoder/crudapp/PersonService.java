de0ffcb31ce7e1abf12da170f07a80e7a6a05033
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-08-14 02:49:07 -0400

    almost finished lab

---
+++ src/main/java/io/zipcoder/crudapp/PersonService.java
@@ -0,0 +1,50 @@
+package io.zipcoder.crudapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class PersonService {
+    private PersonRepository repository;
+    @Autowired
+    public PersonService (PersonRepository repository){
+        this.repository=repository;
+    }
+
+    public Person create(Person person){
+        return repository.save(person);
+    }
+
+    public Person findById(Long id){
+        return repository.findOne(id);
+    }
+
+    public List<Person> findAll(){
+        Iterator<Person> allPerson= (Iterator<Person>) repository.findAll();
+        List<Person> personList = new ArrayList<>();
+        allPerson.forEachRemaining(personList::add);
+        return personList;
+    }
+
+    public Person update(Long id, Person newData){
+        Person personInDatabase = this.findById(id);
+        personInDatabase.setFirstName(newData.getFirstName());
+        personInDatabase.setLastName(newData.getLastName());
+        personInDatabase=repository.save(personInDatabase);
+        return personInDatabase;
+    }
+
+    public Person deleteById(Long id) {
+        Person personDeleted = this.findById(id);
+        repository.delete(personDeleted);
+        return  personDeleted;
+    }
+
+
+
+
+}
