4651b8c4d3d9654fc9d64c9d1222c8a79a041723
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-08-14 03:14:32 -0400

    updated controllers

--- src/main/java/io/zipcoder/crudapp/PersonController.java
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -3,14 +3,16 @@ package io.zipcoder.crudapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-@RestController
-@RequestMapping("/people")
+@Controller
+@RequestMapping("people")
 public class PersonController {
 
     private PersonService service;
@@ -19,48 +21,36 @@ public class PersonController {
     public PersonController(PersonService service){
         this.service= service;
     }
-    @Autowired
-    PersonRepository personRepository;
+//    @Autowired
+//    PersonRepository personRepository;
 
 
     @PostMapping
     public ResponseEntity<Person> createPerson(@RequestBody Person p){ //requestbody is data sent by client to my API
-        return new ResponseEntity<>(personRepository.save(p), HttpStatus.CREATED); //saving person p in personRepository
+        return new ResponseEntity<>(service.create(p), HttpStatus.CREATED); //saving person p in personRepository
     }
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Person> getPerson(@PathVariable("id") Long id){
-        Person person = personRepository.findOne(id);
-        if(person ==null){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<Person> getPerson(@PathVariable Long id){
+        return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity<List<Person>> getPersonList(){
-        List<Person> personList = (List<Person>) personRepository.findAll();
-        if(personList ==null || personList.isEmpty()){
-            return new ResponseEntity<List<Person>>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<List<Person>>(HttpStatus.OK);
+        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
+
     }
 
     @PutMapping ("/{id}")
-    public ResponseEntity<Person> updatePerson(@PathVariable("id") Long id, String firstName, String lastName){
-        Person currentPerson = new Person(firstName, lastName, id);
-        if(personRepository.exists(id)){
-            return new ResponseEntity<>(personRepository.save(currentPerson), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(personRepository.save(currentPerson), HttpStatus.CREATED);
+    public ResponseEntity<Person> updatePerson(@PathVariable Long id, @RequestBody Person p){
+        return new ResponseEntity<>(service.update(id,p), HttpStatus.OK);
     }
 
     @DeleteMapping ("/{id}")
-    public ResponseEntity<Void> DeletePerson(@PathVariable("id") Long id){
-        Person p = personRepository.findOne(id);
-        personRepository.delete(p);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<Person> DeletePerson(@PathVariable Long id){
+        return new ResponseEntity<>(service.deleteById(id), HttpStatus.OK);
+
 
     }
 
